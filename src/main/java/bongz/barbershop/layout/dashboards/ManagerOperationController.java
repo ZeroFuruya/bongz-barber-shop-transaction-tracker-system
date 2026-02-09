c9edb6f1a1f81b7b80c26a05cccc549760504046
Augustus Winchester <99241183+ZeroFuruya@users.noreply.github.com>
2026-02-09 23:17:45 +0800

    TO DO: Make Manger Ops

---
+++ src/main/java/bongz/barbershop/layout/dashboards/ManagerOperationController.java
@@ -0,0 +1,26 @@
+package bongz.barbershop.layout.dashboards;
+
+import bongz.barbershop.App;
+import bongz.barbershop.layout.MainAppController;
+import bongz.barbershop.loader.AppLoader;
+import javafx.fxml.FXML;
+
+public class ManagerOperationController {
+
+    private App app;
+    private MainAppController mainAppController;
+
+    public void load(App app, MainAppController mainAppController) {
+        this.app = app;
+        this.mainAppController = mainAppController;
+    }
+
+    @FXML
+    private void handleLogout() {
+        try {
+            AppLoader.load_app_window(app, app.getMainStage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
